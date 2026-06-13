15d427d0c6b6507b5f40172a8a6c74add1fc13f8
70154216-blip <70154216@student.uol.edu.pk>
2026-06-13 20:02:47 +0500

    Update return a book.sql

--- return a book.sql
+++ return a book.sql
@@ -2,5 +2,4 @@ UPDATE Loans
 SET return_date = CURRENT_DATE 
 WHERE loan_id = 101;
 
--- Don't forget to restore the book inventory:
 UPDATE Books SET available_copies = available_copies + 1 WHERE book_id = 1;
