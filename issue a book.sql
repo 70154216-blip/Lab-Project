2f20367c772f277e05548b9005f9de40aa84811f
70154216-blip <70154216@student.uol.edu.pk>
2026-06-13 20:16:03 +0500

    loans.sql

--- issue a book.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO Loans (book_id, member_id, due_date)
-VALUES (1, 5, DATE_ADD(CURRENT_DATE, INTERVAL 14 DAY));
---
+++ loans.sql
@@ -0,0 +1,2 @@
+INSERT INTO Loans (book_id, member_id, due_date)
+VALUES (1, 5, DATE_ADD(CURRENT_DATE, INTERVAL 14 DAY));
